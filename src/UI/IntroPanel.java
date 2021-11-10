f510cc887f2a0bc481edc12d6a254058fb6d45cc
PatrickWard86 <trixsta1986@live.co.uk>
2021-11-10 11:58:53 +0000

    beginning to build game intoduction

--- src/UI/IntroPanel.java
+++ src/UI/IntroPanel.java
@@ -1,11 +1,18 @@
 package UI;
 
+import UI.Labels.GameIntroLabel;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class IntroPanel extends JPanel {
 
+    GameIntroLabel introLabel;
+
     public IntroPanel() {
         this.setBackground(Color.BLACK);
+        this.introLabel = new GameIntroLabel();
     }
+
+
 }
--- src/UI/Labels/GameIntroLabel.java
+++ src/UI/Labels/GameIntroLabel.java
@@ -1,7 +1,15 @@
 package UI.Labels;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameIntroLabel extends JLabel {
 
+    public GameIntroLabel() {
+        this.setMinimumSize(new Dimension(800, 200));
+        this.setBorder(BorderFactory.createCompoundBorder());
+        this.setBackground(Color.WHITE);
+        this.setVerticalTextPosition(0);
+        this.setVisible(true);
+    }
 }
